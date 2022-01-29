420c5039284fb698f348d659ac2873ac291b9682
David F. Mulcahey <david.mulcahey@me.com>
2022-01-29 17:30:55 -0500

    Controller and type cleanup (#30)
    * update group model when group changes
    
    * use enums in client side controller
    
    * IASZone zone type
    
    * better name
    
    * consistent broadcast format

--- zhaws/server/zigbee/controller.py
+++ zhaws/server/zigbee/controller.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import asyncio
 import logging
 import time
-from typing import TYPE_CHECKING, Any, Optional, Union
+from typing import TYPE_CHECKING, Optional, Union
 
 from bellows.zigbee.application import ControllerApplication
 from serial.serialutil import SerialException
@@ -236,25 +236,28 @@ class Controller:
         device = self._devices.pop(device.ieee, None)
         if device is not None:
             device.on_remove()
-            message: dict[str, Any] = {DEVICE: device.zha_device_info}
-            message[MESSAGE_TYPE] = MessageTypes.EVENT
-            message[EVENT_TYPE] = EventTypes.CONTROLLER_EVENT
-            message[EVENT] = ControllerEvents.DEVICE_REMOVED
-            self.server.client_manager.broadcast(message)
+            self.server.client_manager.broadcast(
+                {
+                    DEVICE: device.zha_device_info,
+                    MESSAGE_TYPE: MessageTypes.EVENT,
+                    EVENT_TYPE: EventTypes.CONTROLLER_EVENT,
+                    EVENT: ControllerEvents.DEVICE_REMOVED,
+                }
+            )
 
     def group_member_removed(self, zigpy_group: ZigpyGroup, endpoint: Endpoint) -> None:
         """Handle zigpy group member removed event."""
         # need to handle endpoint correctly on groups
         group = self.get_or_create_group(zigpy_group)
         group.info("group_member_removed - endpoint: %s", endpoint)
-        self._emit_group_event(group, ControllerEvents.GROUP_MEMBER_REMOVED)
+        self._broadcast_group_event(group, ControllerEvents.GROUP_MEMBER_REMOVED)
 
     def group_member_added(self, zigpy_group: ZigpyGroup, endpoint: Endpoint) -> None:
         """Handle zigpy group member added event."""
         # need to handle endpoint correctly on groups
         group = self.get_or_create_group(zigpy_group)
         group.info("group_member_added - endpoint: %s", endpoint)
-        self._emit_group_event(group, ControllerEvents.GROUP_MEMBER_ADDED)
+        self._broadcast_group_event(group, ControllerEvents.GROUP_MEMBER_ADDED)
         if len(group.members) > 1:
             # we need to do this because there wasn't already a group entity to remove and re-add
             discovery.GROUP_PROBE.discover_group_entities(group)
@@ -263,7 +266,7 @@ class Controller:
         """Handle zigpy group added event."""
         group = self.get_or_create_group(zigpy_group)
         group.info("group_added")
-        self._emit_group_event(group, ControllerEvents.GROUP_ADDED)
+        self._broadcast_group_event(group, ControllerEvents.GROUP_ADDED)
 
     def group_removed(self, zigpy_group: ZigpyGroup) -> None:
         """Handle zigpy group removed event."""
@@ -271,7 +274,7 @@ class Controller:
         group = self._groups.pop(zigpy_group.group_id, None)
         if group is not None:
             group.info("group_removed")
-            self._emit_group_event(group, ControllerEvents.GROUP_REMOVED)
+            self._broadcast_group_event(group, ControllerEvents.GROUP_REMOVED)
 
     async def async_device_initialized(self, device: ZigpyDeviceType) -> None:
         """Handle device joined and basic information discovered (async)."""
@@ -304,13 +307,16 @@ class Controller:
             )
             await self._async_device_joined(zha_device)
 
-        message: dict[str, Any] = {DEVICE: zha_device.zha_device_info}
-        message["new_join"] = not is_rejoin
-        message[PAIRING_STATUS] = DevicePairingStatus.INITIALIZED
-        message[MESSAGE_TYPE] = MessageTypes.EVENT
-        message[EVENT_TYPE] = EventTypes.CONTROLLER_EVENT
-        message[EVENT] = ControllerEvents.DEVICE_FULLY_INITIALIZED
-        self.server.client_manager.broadcast(message)
+        self.server.client_manager.broadcast(
+            {
+                DEVICE: zha_device.zha_device_info,
+                "new_join": not is_rejoin,
+                PAIRING_STATUS: DevicePairingStatus.INITIALIZED,
+                MESSAGE_TYPE: MessageTypes.EVENT,
+                EVENT_TYPE: EventTypes.CONTROLLER_EVENT,
+                EVENT: ControllerEvents.DEVICE_FULLY_INITIALIZED,
+            }
+        )
 
     def get_or_create_device(self, zigpy_device: ZigpyDeviceType) -> Device:
         """Get or create a device."""
@@ -329,13 +335,16 @@ class Controller:
 
     async def _async_device_joined(self, device: Device) -> None:
         device.available = True
-        message: dict[str, Any] = {DEVICE: device.device_info}
         await device.async_configure()
-        message[PAIRING_STATUS] = DevicePairingStatus.CONFIGURED
-        message[MESSAGE_TYPE] = MessageTypes.EVENT
-        message[EVENT_TYPE] = EventTypes.CONTROLLER_EVENT
-        message[EVENT] = ControllerEvents.DEVICE_CONFIGURED
-        self.server.client_manager.broadcast(message)
+        self.server.client_manager.broadcast(
+            {
+                DEVICE: device.device_info,
+                PAIRING_STATUS: DevicePairingStatus.CONFIGURED,
+                MESSAGE_TYPE: MessageTypes.EVENT,
+                EVENT_TYPE: EventTypes.CONTROLLER_EVENT,
+                EVENT: ControllerEvents.DEVICE_CONFIGURED,
+            }
+        )
         await device.async_initialize(from_cache=False)
         self.create_platform_entities()
 
@@ -347,12 +356,15 @@ class Controller:
         )
         # we don't have to do this on a nwk swap but we don't have a way to tell currently
         await device.async_configure()
-        message: dict[str, Any] = {DEVICE: device.device_info}
-        message[PAIRING_STATUS] = DevicePairingStatus.CONFIGURED
-        message[MESSAGE_TYPE] = MessageTypes.EVENT
-        message[EVENT_TYPE] = EventTypes.CONTROLLER_EVENT
-        message[EVENT] = ControllerEvents.DEVICE_CONFIGURED
-        self.server.client_manager.broadcast(message)
+        self.server.client_manager.broadcast(
+            {
+                DEVICE: device.device_info,
+                PAIRING_STATUS: DevicePairingStatus.CONFIGURED,
+                MESSAGE_TYPE: MessageTypes.EVENT,
+                EVENT_TYPE: EventTypes.CONTROLLER_EVENT,
+                EVENT: ControllerEvents.DEVICE_CONFIGURED,
+            }
+        )
         # force async_initialize() to fire so don't explicitly call it
         device.available = False
         device.update_available(True)
@@ -412,10 +424,13 @@ class Controller:
                 await asyncio.gather(*tasks)
         self.application_controller.groups.pop(group_id)
 
-    def _emit_group_event(self, group: Group, event: str) -> None:
-        """Emit group event."""
-        message: dict[str, Any] = {"group": group.to_json()}
-        message[MESSAGE_TYPE] = MessageTypes.EVENT
-        message[EVENT_TYPE] = EventTypes.CONTROLLER_EVENT
-        message[EVENT] = event
-        self.server.client_manager.broadcast(message)
+    def _broadcast_group_event(self, group: Group, event: str) -> None:
+        """Broadcast group event."""
+        self.server.client_manager.broadcast(
+            {
+                "group": group.to_json(),
+                MESSAGE_TYPE: MessageTypes.EVENT,
+                EVENT_TYPE: EventTypes.CONTROLLER_EVENT,
+                EVENT: event,
+            }
+        )
